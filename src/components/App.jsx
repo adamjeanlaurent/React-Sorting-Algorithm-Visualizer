2c4f73b6523e747008a17c2f275d1e29dfb84f0f
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2020-02-16 22:07:44 -0500

    display bars

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,11 @@
-import React, {useState} from 'react';
-import '../styles/App.css';
-import SortingVisualizer from './SortingVisualizer';
+import React, { useState } from "react";
+import "../styles/App.css";
+import SortingVisualizer from "./SortingVisualizer";
 
 export default function App() {
-  return (
-    <div className="App">
-      <SortingVisualizer/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <SortingVisualizer />
+        </div>
+    );
 }
-
--- src/components/SortingVisualizer.jsx
+++ src/components/SortingVisualizer.jsx
@@ -1,5 +1,32 @@
-import React, {useState} from 'react';
- 
+import React, { useState } from "react";
+import "../styles/App.css";
+
 export default function SortingVisualizer(props) {
-    return "hello";
+    const UPPER_BOUND = 200;
+    const ARRAY_SIZE = 200;
+
+    const [array, updateArray] = useState(fillArrayWithRandomValues);
+
+    function fillArrayWithRandomValues() {
+        // updateArray([]);
+        let tempArray = [];
+        for (let i = 0; i < ARRAY_SIZE; i++) {
+            tempArray.push(Math.floor(Math.random() * UPPER_BOUND) + 5);
+        }
+        return tempArray;
+    }
+
+    return (
+        <div className="array-container">
+            {array.map((value, idx) => {
+                return (
+                    <div
+                        style={{ height: `${value * 2}px` }}
+                        className="array-bar"
+                        key={idx}
+                    ></div>
+                );
+            })}
+        </div>
+    );
 }
